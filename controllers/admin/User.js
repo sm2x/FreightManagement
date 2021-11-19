16a054335b060629beaa4e65461a2e19eba4c40e
Anirban Karmakar <karmakar_anirban89@outlook.com>
2021-11-19 11:05:21 +0530

    Admin get user apis

---
+++ controllers/admin/User.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+const USER = require('../../models/user');
+
+var viewAllUsers = async (req, res) => {
+    var users = await USER.find().exec();
+
+    if (users.length > 0) {
+        return res.status(200).json({
+            status: true,
+            message: "Data successfully get.",
+            data: users
+        });
+    }
+    else {
+        return res.status(200).json({
+            status: true,
+            message: "No registered user found.",
+            data: null
+        });
+    }
+}
+
+var viewUserById = async (req, res) => {
+    var id = req.params.id;
+
+    return USER.findOne(
+        { _id: mongoose.Types.ObjectId(id) }
+    )
+        .then(data => {
+            res.status(200).json({
+                status: true,
+                message: "Data successfully get.",
+                data: data
+            });
+        })
+        .catch(err=>{
+            res.status(500).json({
+                status: false,
+                message: "Invalid id. Server error.",
+                error: err.message
+            });
+        });
+}
+
+module.exports = {
+    viewAllUsers,
+    viewUserById,
+    viewUsersByCompany
+}
--- routes/v1/admin.js
+++ routes/v1/admin.js
@@ -2,7 +2,11 @@ var express = require('express');
 
 var router = express.Router();
 
+const USER = require('../../controllers/admin/User');
 const USER_PERMISSIONS = require('../../controllers/admin/UserPermissions');
+
+router.get('/users', USER.viewAllUsers);
+router.get('/users/:id', USER.viewUserById);
 
 router.post('/user-permissions', USER_PERMISSIONS.addPermissions);
 
